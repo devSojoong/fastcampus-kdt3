aecbd0772826b73885357bbc7aa147402afe4932
devSojoong <sojoong.dev@gmail.com>
2022-09-20 01:05:17 +0900

    Feat. Java Basement Study
    (print, printf, etc..)

---
+++ java/src/me/java/comment/Main.java
@@ -0,0 +1,25 @@
+package me.java.comment;
+
+public class Main {
+    public static void main (String[] args){
+        /*
+        여러 줄 주석
+         */
+
+        // 한 줄 주성
+        //ex) Hello Java World 출력, 윤소중, 2022-09-19
+
+        //문장 끝에는 세미콜론 작성
+        System.out.println("Hello Java World - 1");
+        System.out.println("Hello Java World - 2");
+        System.out.println("Hello Java World - 3");
+        System.out.println("Hello Java World - 4");
+        System.out.println("Hello Java World - 5");
+
+        //콘솔에 출력하는 함수
+        System.out.println("1"); //(문장 + 엔터)
+        System.out.print("2"); // 문장만 출력
+        System.out.print("3"); // 포맷에 맞춰 출력
+        System.out.println();
+    }
+}
---
+++ java/src/me/java/variables/Main.java
@@ -0,0 +1,10 @@
+package me.java.variables;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);   //데이터를 담고 있는 변수.
+        System.out.println('a'); //문자
+        System.out.println("a"); //문자열
+    }
+}
